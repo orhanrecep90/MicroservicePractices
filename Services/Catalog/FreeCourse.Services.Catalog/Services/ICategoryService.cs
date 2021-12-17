1960644d64de3541a14ec7e07fc9ec4b95961935
recep.orhan <orhanrecep90@gmail.com>
2021-12-17 20:48:40 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+   public interface ICategoryService
+    {
+        public  Task<Response<List<CategoryDto>>> GetAllAsync();
+        public Task<Response<CategoryDto>> CreateAsync(CategoryDto category);
+
+        public  Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
